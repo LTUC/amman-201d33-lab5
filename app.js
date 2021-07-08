868ce8e9acb1129fa6574c45cffc45796bba6ded
MasteRminD6666 <rami-zaitoun99@hotmail.com>
2021-07-08 15:05:08 +0300

    Problem 4 Solved

--- app.js
+++ app.js
@@ -80,12 +80,22 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+ let firstN , secondN , thirdN
+ firstN =  sumArr[0];
+ secondN = sumArr[1];
+ thirdN =  sumArr[2];
+ let sumofFirst = sum(firstN,secondN)[0];
+ let sumofThird = sum(sumofFirst,thirdN)[0];
+ let SecondElement = `${firstN},${secondN},${thirdN} was passed in as an array of numbers, and ${sumofThird} is their sum.`
+ let array = [sumofThird,SecondElement]
+ return array
+ 
 }
+//sumArray(testArray)
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
