3df5c22b5c228383c9248dd651fae8a86f35bdbd
MohammadAljadayh <mjedayh@gmail.com>
2021-07-08 14:38:02 +0300

    Solved Problem 4

--- app.js
+++ app.js
@@ -70,11 +70,19 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
+let testArray =[2, 3, 4]; //eslint-disable-line
+function sumArray(sumArr) { 
+  //eslint-disable-line
+  let testArray1=sum(sumArr[0],sumArr[1])[0];
+  let testArray2=sum(testArray1,sumArr[2])[0];
+let testArray3=`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${testArray2} is their sum.`
+  return[testArray2,testArray3];
 }
+// Here is the test for sumArray(); uncomment it to run it
+
+console.log (sumArray(testArray));
+testSumArray(testArray);
 
 // Here is the test for sumArray(); uncomment it to run it
 
