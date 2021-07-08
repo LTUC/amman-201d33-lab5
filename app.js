e4d4528f6b81023b3d321d5fca84fdbb353afd4e
odai-sadaqa <sadaqa.odai@yahoo.com>
2021-07-08 16:47:20 +0300

    finished problem4

--- app.js
+++ app.js
@@ -86,14 +86,36 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
+// console.log(testArray, 'hello');
 
 function sumArray(sumArr) { //eslint-disable-line
+  // console.log(testArray[0],'index 0');
+  // console.log(testArray[1],'index 1');
+  // console.log(testArray[2],'index 2');
+  // let result1= sum(testArray[0],testArray[1]);
+  // console.log(result1, 'here');
+  // let result2= sum(result1[0], testArray[2])
+  // console.log(result2,'hello');
+  //  let test3= [testArray[0],testArray[1],testArray[2],`was passed in as an array of numbers,and ${result2[0]} is their sum.`];
+  // let test3= (sum,`was passed in as an array of numbers,and ${result2[0]} is their sum.ok`);
+  // let test3= (testArray[0],testArray[1],testArray[2]);
+  // console.log(test3[0],test3[1],test3[2],`was passed in as an array of numbers, and ${result2[0]} is their sum.`);
+  // // return[testArray[0],testArray[1],testArray[2],`was passed in as an array of numbers, and ${result2[0]} is their sum.`]
+  // return[result2,`${testArray[0]},${testArray[1]},${testArray[2]}was passed in as an array of numbers, and ${result2[0]} is their sum.`]
 
+  let result = 0;
+  for(let i=0;i<sumArr.length;i++){
+    result = sum(result,sumArr[i])[0];
+  }
+  return [result, `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${result} is their sum.`]
 }
+
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
