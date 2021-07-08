7794d3d35d172a5d5dec44b651706bfe82cd159d
Haneen Haashlamoun <haneen.hashlamoun@gmail.com>
2021-07-08 15:58:25 +0300

    addProblem2

--- app.js
+++ app.js
@@ -53,16 +53,15 @@ Test this function by hand in the console to get it working, and when you think 
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
   let sum1 = sum(sum(a, b)[0], c)[0];
-  
+
   let prod1 = multiply(multiply(a, b)[0], c)[0];
-  
-  return [sum1,prod1,`${a} and ${b} and ${c} sum to ${sum1}.`,`The product of ${a} and ${b} and ${c} is ${prod1}.`]
-  
+
+  return [sum1, prod1, `${a} and ${b} and ${c} sum to ${sum1}.`, `The product of ${a} and ${b} and ${c} is ${prod1}.`]
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
-// testSumAndMultiply(4, 7, 5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -80,13 +79,13 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
-
+  let sum1 = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];  
+  return [sum1,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and 9 is their sum.`]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -130,6 +129,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+
 
 }
 
