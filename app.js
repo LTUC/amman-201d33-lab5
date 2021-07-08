8501d969972f7e6eae42d820ad9c7ca544c22325
RihanFoudeh <Rihanfoudeh@gmail.com>
2021-07-08 13:41:05 +0300

    solve problem 5

--- app.js
+++ app.js
@@ -77,12 +77,16 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let productSumArray = sumArr[0];
+  for (let i = 1; i < sumArr.length ; i++) {
+    productSumArray = sum(productSumArray,sumArr[i])[0];
+  }
+  return [productSumArray , `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${productSumArray} is their sum.`]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -98,11 +102,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let productMultiplyArray = multArr[0];
+  for (let i = 1; i < multArr.length ; i++) {
+    productMultiplyArray = multiply(productMultiplyArray,multArr[i])[0];
+  }
+  return [productMultiplyArray , `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${productMultiplyArray}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
